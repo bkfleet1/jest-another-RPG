928a1eb2095d6b3598ae5fae63d7521daedaac68
bkfleet1 <bkfleet1@gmail.com>
2022-01-25 18:36:27 -0600

    lesson 10.3.3 adds get health stats, isAlive, reduce health tests

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,8 +1,7 @@
 const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
-jest.mock('../lib/Potion.js');
 
-console.log(new Potion());
+jest.mock('../lib/Potion.js');
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -29,4 +28,33 @@ test('creates a player object', () => {
     player.inventory = [];
   
     expect(player.getInventory()).toEqual(false);
+  });
+
+  test("gets player's health value", () => {
+    const player = new Player('Dave');
+  
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+  });
+
+  test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+  
+    expect(player.isAlive()).toBeTruthy();
+  
+    player.health = 0;
+  
+    expect(player.isAlive()).toBeFalsy();
+  });
+
+  test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+  
+    player.reduceHealth(5);
+  
+    expect(player.health).toBe(oldHealth - 5);
+  
+    player.reduceHealth(99999);
+  
+    expect(player.health).toBe(0);
   });
